b1a7fa257423b18dfb953fbe8b552edf665f640d
Jheave <jimenezjheave123@gmail.com>
2021-02-23 02:29:46 +0800

    fix some bug

--- Practice.java
+++ Practice.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
 public class lbRecActTwo_Jimenez {
-    public static int Strlen(String s){
-        if (s.equals("")) 
+    public static int Strlen(String l){
+        if (l.equals("")) 
             return 0; 
         else
-            return Strlen(s.substring(1)) + 1; 
- 
+            return Strlen(l.substring(1)) + 1; 
     }
+    public static void main(String[] args)  
+    { 
+ 
+        System.out.print("Enter a String: ");
+        Scanner scan = new Scanner(System.in);
+        String l = scan.nextLine();
+        scan.close();
+        System.out.println("Length of the string is: " + Strlen(l));
+    } 
 }
